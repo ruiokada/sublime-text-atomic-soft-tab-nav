97e67d8274db14fc1366da86c3d5a067b20e8a5c
Rui Okada <ruisokada@gmail.com>
2022-03-17 21:45:11 -0700

    Add by lines nav feature and demos

--- README.md
+++ README.md
@@ -1,14 +1,30 @@
-# Sublime Text Soft Tab Nav
+# Sublime Text Atomic Soft Tab Nav
 
 In Sublime Text, soft tabs (multiple spaces used as tabs) require as many arrow key presses as there are spaces to navigate. The purpose of this package is to make it so that soft tabs can be navigated just like tabs when using the arrow keys.
+
+![Standard Demo](https://raw.githubusercontent.com/ruiokada/sublime-text-atomic-soft-tab-nav/assets/demo.gif)
+
+Set `astn_enable_line_nav` to `true` in your Sublime Text settings file to enable atomic soft tabs when navigation by lines:
+
+![With 'astn_enable_line_nav' True Demo](https://raw.githubusercontent.com/ruiokada/sublime-text-atomic-soft-tab-nav/assets/demo-by-lines.gif)
+
+## Manual Install
+
+    git clone https://github.com/ruiokada/sublime-text-atomic-soft-tab-nav.git "Atomic Soft Tab Nav"
+
+By default, Sublime Text does not visually distinguish tabs and soft tabs so it is recommended to set `draw_white_space` to `all` in your settings file:
+
+    {"draw_white_space": "all"}
 
 ## Functions
 
 The functionality of this package is simple. When the text cursor encounters a soft tab during arrow key input, this plugin will modify the arrow key input accordingly. The following are the arrow key input modification details:
 
 * Soft tabs are assumed to appear only at the beginning of lines so multiple spaces that do not span to the beginning of the line are ignored.
-* If the cursor is on the edge of a soft tab, then the arrow keys move the cursor as if the soft tabs were tabs.
-* If the cursor is in the middle of a soft tab, then the arrow keys move the cursor as if the soft tab is multiple spaces until either end of the soft tab is reached.
+* If the cursor is on the edge of a soft tab, then the left/right arrow keys move the cursor as if the soft tabs were tabs.
+* If the cursor is in the middle of a soft tab, then the left/right arrow keys move the cursor as if the soft tab is multiple spaces until either end of the soft tab is reached.
+* If `astn_enable_line_nav` is set to true in settings then the up/down arrow keys will also treat soft tabs in a similar manner.
 * The above modifications are applied even in the case when the cursor is making a text selection.
+* The above modifications will not be applied if there are multiple active cursors.
 
 If the spaces as tabs function is disabled in the current file view then this plugin will be disabled. The size of soft tabs depends on the tab size of the current file view. If there are multiple cursors, then navigation input will not be modified.
--- soft_tab_nav.py
+++ soft_tab_nav.py
@@ -5,6 +5,7 @@ import sublime_plugin
 class SoftTabNavListener(sublime_plugin.EventListener):
     view_settings = None
 
+
     def on_activated(self, view):
         self.view_settings = view.settings()
 
@@ -12,15 +13,14 @@ class SoftTabNavListener(sublime_plugin.EventListener):
         if not self.view_settings.get('translate_tabs_to_spaces'):
             return None
 
-        if (command_name != "move" or (
-                'by' in args and args['by'] != 'characters')):
+        if (command_name != "move"):
             return None
 
         selection = view.sel()
         if len(view.sel()) != 1:
             return None
 
-        has_active_selection = min([abs(x.b - x.a) for x in selection]) > 1
+        has_active_selection = min([abs(x.b - x.a) for x in selection]) >= 1
         is_extending_selection = 'extend' in args and args['extend']
         if has_active_selection and not is_extending_selection:
             return None
@@ -29,49 +29,97 @@ class SoftTabNavListener(sublime_plugin.EventListener):
         selection_end = selection[0].b
         tab_size = self.view_settings.get('tab_size')
 
-        beginning_of_cursor_line = view.line(selection_end).a
-
-        selection_is_at_end_of_tab_sequence = (
-            (selection_end - beginning_of_cursor_line) % tab_size == 0 and
-            view.substr(sublime.Region(
+        if args.get('by') == 'characters':
+            """
+            Handles the case when the cursor will enter a soft tab.
+             The author is unaware of how to cancel keyboard input
+             commands so here we programatically move the cursor to the
+             next edge of soft tab the cursor will enter and then move the
+             cursor back/forward one character to "cancel" the original
+             left/right key input.
+            """
+            beginning_of_cursor_line = view.line(selection_end).a
+            selection_is_at_end_of_space_sequence = view.substr(sublime.Region(
                 beginning_of_cursor_line,
                 selection_end
             )) == " " * (selection_end - beginning_of_cursor_line)
-        )
-        if not selection_is_at_end_of_tab_sequence:
-            return None
 
-        move_direction = 1 if args['forward'] else -1
+            if not selection_is_at_end_of_space_sequence:
+                return None
 
-        text_in_next_tab_block = view.substr(sublime.Region(
-            selection_end,
-            selection_end + move_direction * tab_size
-        ))
-        cursor_will_move_into_tab = (
-            text_in_next_tab_block == " " * tab_size
-        )
-        if not cursor_will_move_into_tab:
-            return None
+            position_in_block = (selection_end - beginning_of_cursor_line) % tab_size
+            if args.get('forward') is True:
+                remaining_text_in_current_tab_block = view.substr(sublime.Region(
+                    selection_end,
+                    selection_end + (tab_size - position_in_block)
+                ))
 
-        if args['forward']:
-            next_selection_overlaps_start = (
-                selection_end < selection_start and
-                selection_start < selection_end + tab_size
+                if remaining_text_in_current_tab_block == " " * (tab_size - position_in_block):
+                    next_selection_end = selection_end + (tab_size - position_in_block) - 1
+                else:
+                    return None
+            elif args.get('forward') is False:
+                if selection_end == beginning_of_cursor_line:
+                    return None
+                if position_in_block == 0:
+                    next_selection_end = selection_end - tab_size + 1
+                else:
+                    next_selection_end = selection_end - position_in_block + 1
+
+            next_selection_start = (
+                selection_start if is_extending_selection else next_selection_end
             )
-        else:
-            next_selection_overlaps_start = (
-                selection_start < selection_end and
-                selection_end - tab_size < selection_start
-            )
-        if next_selection_overlaps_start:
-            return None
+            selection.clear()
+            selection.add(sublime.Region(
+                next_selection_start,
+                next_selection_end
+            ))
+        elif args.get('by') == 'lines' and self.view_settings.get('astn_enable_line_nav'):
+            """
+            Handles the case when the cursor moves onto a soft tab
+             from a line above or below. In this case we programatically
+             move the cursor to the next line and then replace the
+             original keyboard input with left/right keydown depending on
+             where the cursor is in the current soft tab block. This will
+             trigger the previous if/else case and move the cursor onto
+             the edge of the soft tab.
+            """
 
-        next_selection_end = selection_end + move_direction * (tab_size - 1)
-        next_selection_start = (
-            selection_start if is_extending_selection else next_selection_end
-        )
-        selection.clear()
-        selection.add(sublime.Region(
-            next_selection_start,
-            next_selection_end
-        ))
+            selection_line_start = view.line(selection_end).a
+            selection_line_end = view.line(selection_end).b
+            if args.get('forward') is True:
+                next_line_start = selection_line_end + 1
+                next_line_end = view.line(next_line_start).b
+            elif args.get('forward') is False:
+                next_line_end = selection_line_start - 1
+                next_line_start = view.line(next_line_end).a
+            position_in_block = (selection_end - selection_line_start) % tab_size
+
+            # If next line length is smaller than position in current
+            # line then the cursor can't hit a soft tab.
+            if next_line_end - next_line_start < (tab_size - position_in_block) + selection_end - selection_line_start:
+                return None
+
+            next_selection_end = next_line_start + (selection_end - selection_line_start)
+            next_selection_end_is_at_end_of_space_sequence = view.substr(sublime.Region(
+                next_line_start,
+                next_selection_end
+            )) == " " * (selection_end - selection_line_start)
+            if position_in_block != 0 and next_selection_end_is_at_end_of_space_sequence:
+                remaining_text_in_next_tab_block = view.substr(sublime.Region(
+                    next_selection_end,
+                    next_selection_end + (tab_size - position_in_block)
+                ))
+                if remaining_text_in_next_tab_block == " " * (tab_size - position_in_block):
+                    next_selection_start = (
+                        selection_start if is_extending_selection else next_selection_end
+                    )
+                    selection.clear()
+                    selection.add(sublime.Region(
+                        next_selection_start, next_selection_end
+                    ))
+                    return ('move', {
+                        'extend': is_extending_selection,
+                        'forward': position_in_block >= tab_size // 2,
+                        'by': 'characters'
+                    })
